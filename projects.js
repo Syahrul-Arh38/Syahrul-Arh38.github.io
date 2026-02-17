50bb73c21105e0f480dbdfe63f10e16ee79a7a57
Syahrul-Arh38 <syahrulihzaarhamna038@gmail.com>
2026-02-17 22:08:57 +0700

    Tambahkan sedikit detai;

---
+++ projects.js
@@ -0,0 +1,67 @@
+const projects = [
+    {
+        id: "poster-psb",
+        title: "Poster Penerimaan Santri Baru 2024",
+        image: "images/DesainGrafis/Poster PSB.png",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        tools: "Canva",
+        year: "2025"
+    },
+    {
+        id: "poster-pancasila",
+        title: "Poster peringatan pancasila",
+        image: "images/DesainGrafis/Pamflet Pondok 2024 (3).png",
+        desc: "Identitas visual",
+        tools: "Adobe Illustrator, Photoshop",
+        year: "2025"
+    },
+    {
+        id: "poster-hsn",
+        title: "Poster Event HSN",
+        image: "images/DesainGrafis/Pamflet Pondok HSN 2025.png",
+        desc: "Desain poster promosi Hari Santri Nasional 2025.",
+        tools: "Adobe Illustrator, Photoshop",
+        year: "2025"
+    },
+    {
+        id: "twibbon_hsn",
+        title: "Desain Twibbon Hari Santri 2025",
+        image: "images/DesainGrafis/hsn-pprt-2025-2a060d65-2114-4622-aaed-2ba778ffb29a.jpg",
+        desc: 'Bingkai foto digital (twibbon) untuk peringatan Hari Santri Nasional 2025. Desain menggunakan elemen grafis melengkung yang dinamis dengan kombinasi warna hijau, ungu, merah muda, dan kuning yang kontras di atas latar belakang transparan. Dilengkapi dengan teks slogan "Mengawal Indonesia Merdeka Menuju Peradaban Dunia" yang menggunakan tipografi sans-serif modern yang bersih. Sangat cocok untuk kampanye media sosial, bingkai profil, dan identitas visual acara keagamaan.',
+        tools: "Adobe Illustrator, Photoshop",
+        year: "2025"
+    },
+    {
+        id: "love-envelope",
+        title: "Ikon Surat Cinta Romantis Merah Muda.",
+        image: "images/DesainGrafis/Project.png",
+        desc: "Grafik vektor amplop surat bertema cinta dengan detail hati kecil. Sangat ideal untuk desain kartu ucapan, aset antarmuka aplikasi (UI), media sosial bertema romansa, dan dekorasi acara spesial.",
+        tools: "Adobe Illustrator",
+        year: "2024"
+    },
+    {
+        id: "logo-cms",
+        title: "Logo Cepagan Mini Soccer",
+        image: "images/DesainGrafis/cms-01.png",
+        desc: "Proyek ini melibatkan perancangan logo resmi untuk kompetisi 'Cepagan Mini Soccer 2024'. Tujuan utamanya adalah menciptakan identitas visual yang memancarkan energi, sportivitas, dan semangat komunitas lokal di Desa Cepagan melalui simbolisme grafis yang modern.",
+        tools: "Adobe Illustrator, Photoshop",
+        year: "2024"
+    },
+    {
+        id: "cover-bukuKKN",
+        title: "Harmoni Nusantara",
+        image: "images/DesainGrafis/Biru Illustrasi Buku Agenda Sekolah A4 Document (1).png",
+        desc: "Proyek ini mencakup perancangan identitas visual dan desain sampul untuk laporan atau buku dokumentasi kegiatan KKN-MB Posko 83 UIN Walisongo Semarang. Judul 'Harmoni Nusantara' merefleksikan integrasi antara tradisi lokal, edukasi, dan penguatan komunitas di Desa Cepagan.",
+        tools: "Adobe Illustrator, Photoshop",
+        year: "2024"
+    },
+    {
+        id: "vector-arlecchino",
+        title: "The Knave",
+        image: "images/DesainGrafis/Proyek_1.png",
+        desc: "Proyek ini merupakan sebuah fan art digital yang mengeksplorasi karakter Arlecchino dari game Genshin Impact. Fokus utama dari karya ini adalah mentransformasi desain karakter yang kompleks ke dalam gaya Flat Vector Art dengan sentuhan Cell Shading yang tajam dan bersih.",
+        tools: "Adobe Illustrator, Photoshop",
+        year: "2025"
+    }
+  ];
+  
